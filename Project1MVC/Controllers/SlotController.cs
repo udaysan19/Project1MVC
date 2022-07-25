bda2b5e5b4c74db934bc1daf329f5b8fb69b2cba
Uday <uday.isukapalli@sandhata.com>
2022-07-25 11:54:49 +0530

    Add project files.

---
+++ Project1MVC/Controllers/SlotController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Project1MVC.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Project1MVC.Controllers
+{
+    public class SlotController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:44321/api");
+        //In asp.net MVC httpclient is available to consume Web API
+        //Take reference of httpclient
+        //Click on reference and add reference
+        // GET: Port
+        HttpClient client;
+
+        public SlotController()
+        {  //intialize http client object 
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+
+        }
+        public IActionResult Index()
+        {//Using this http client obj call port web API
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/port").Result;
+            List<Portslot> slotModel = new List<Portslot>();
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                //Deserialise json data into slotModel for
+                //this newton soft
+                //for serialization and desserialization
+                //Install from nuget -Newton soft package - Newtonsoft.Json
+                slotModel = JsonConvert.DeserializeObject<List<Portslot>>(data);
+            }
+            return View(slotModel);
+        }
+    }
+}
---
+++ Project1MVC/Models/Portuser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project1MVC.Models
+{
+
+
+    public class Portuser
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long? Phone { get; set; }
+        public long? Cost { get; set; }
+        public DateTime? Startdate { get; set; }
+        public DateTime? Endtime { get; set; }
+        public string Usertype { get; set; }
+    }
+
+}
